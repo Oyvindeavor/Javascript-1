50a8f0da729e2a24a8ad0a2f2a91d033199fb329
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-12 13:34:39 +0100

    Create fetchdata.mjs

---
+++ scripts/utils/fetchdata.mjs
@@ -0,0 +1,18 @@
+"use strict";
+
+const api_url = "https://api.noroff.dev/api/v1/gamehub";
+
+// Function to fetch data from the API
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching data from API:", error.message);
+  }
+}
+
+export { fetchData, api_url };
