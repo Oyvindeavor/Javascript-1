6384b2fade2b4dfcb986605a73f6ab85f8e192bf
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-15 13:36:43 +0100

    Update product.mjs

--- scripts/product/product.mjs
+++ scripts/product/product.mjs
@@ -4,23 +4,33 @@ import { api_url, fetchData } from "../utils/fetchdata.mjs";
 // Add class to element, create class.
 import { displayProduct } from "../utils/domUtils.mjs";
 
+
 // Gets the ID from the url
-function getIdFromUrl() {
+export function getIdFromUrl() {
   const filterUrl = new URLSearchParams(window.location.search);
   const productId = filterUrl.get("productId");
+  console.log(productId);
   return productId;
 }
 
 async function getAndDisplayProduct() {
   try {
     const productId = getIdFromUrl();
+    if (!productId) {
+      throw new Error('Product ID not found in URL');
+    }
     const product = await fetchData(`${api_url}/${productId}`);
+    if (!product) {
+      throw new Error('Product data not found');
+    }
     displayProduct(product);
   } catch (error) {
-    console.error("Error fetching the data, error code: ", error);
+    console.error("Error fetching or displaying product:", error);
   }
 }
 
 
 getAndDisplayProduct();
+
+
 
