18d851b47b89d23015cbb942196c3dabc9871ce9
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-15 13:36:54 +0100

    Update fetchdata.mjs

--- scripts/utils/fetchdata.mjs
+++ scripts/utils/fetchdata.mjs
@@ -1,19 +1,24 @@
 "use strict";
 
-const api_url = "https://api.noroff.dev/api/v1/gamehub";
+import { showLoadingSpinner } from "./loadingSpinner.mjs";
+import { hideLoadingSpinner } from "./loadingSpinner.mjs";
+
+export const api_url = "https://api.noroff.dev/api/v1/gamehub";
 
 // Function to fetch data from the API
-async function fetchData(url) {
+export async function fetchData(url) {
   try {
+    showLoadingSpinner();
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    hideLoadingSpinner();
     return response.json();
   } catch (error) {
     console.error("Error fetching data from API:", error.message);
   }
 }
 
-export { fetchData, api_url };
+
 
