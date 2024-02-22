d35a54d983d34ee393c5d0203fd1967f7a24bf96
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-22 12:01:43 +0100

    Update cart.mjs

--- scripts/utils/cart.mjs
+++ scripts/utils/cart.mjs
@@ -1,20 +1,18 @@
-
 // Add product(s) to cart
 export function addToCart(product) {
   // Get the existing items from storage
   let cartItems = getCartItemsFromStorage();
 
-  const existingItemIndex = cartItems.findIndex(function(item) {
-      return item.id === product.id;
+  const existingItemIndex = cartItems.findIndex(function (item) {
+    return item.id === product.id;
   });
   // Check if the product exists in the cart
   if (existingItemIndex !== -1) {
     // If the product exist increment by 1
-      cartItems[existingItemIndex].quantity++;
+    cartItems[existingItemIndex].quantity++;
   } else {
-
     // If the product does not exist add it to the cart with a quantity of 1
-      cartItems.push({ id: product.id, quantity: 1 });
+    cartItems.push({ id: product.id, quantity: 1 });
   }
   // Update the new array in storage
   updateCartInStorage(cartItems);
@@ -29,42 +27,37 @@ export function updateCartInStorage(cartItems) {
   localStorage.setItem("cart", JSON.stringify(cartItems));
 }
 
-
-// This gets the products from cart > parses it so i can use the objects > If blocks to return an empty array. 
+// This gets the products from cart > parses it so i can use the objects > If blocks to return an empty array.
 export function getCartItemsFromStorage() {
   try {
-      const cartItemsString = localStorage.getItem("cart");
-      if (!cartItemsString) return []; 
-      return JSON.parse(cartItemsString);
+    const cartItemsString = localStorage.getItem("cart");
+    if (!cartItemsString) return [];
+    return JSON.parse(cartItemsString);
   } catch (error) {
-      console.error("Error retrieving cart items from storage:", error);
-      return []; 
+    console.error("Error retrieving cart items from storage:", error);
+    return [];
   }
 }
 
-
 // Clears all carts from local storage
-export function clearAllCartItemsFromStorage(){
+export function clearAllCartItemsFromStorage() {
   localStorage.setItem("cart", JSON.stringify([]));
 }
-
 
 // Gets all the products added to local storage > loops through them > adds it to variable then returns it
 export function cartCounter() {
   let cartItems = getCartItemsFromStorage();
   let totalQuantity = 0;
   for (const item of cartItems) {
-      totalQuantity += item.quantity;
+    totalQuantity += item.quantity;
   }
   return totalQuantity;
 }
 
-
-// Updates the cart icon to show number of items in the cart > get the total quantity in local storage from cartCounter() 
+// Updates the cart icon to show number of items in the cart > get the total quantity in local storage from cartCounter()
 // Select the counter graphic > change the text to the quantity
-export function updateCartIcon(){
+export function updateCartIcon() {
   const cartQuantity = cartCounter();
   const cartIconCounter = document.querySelector(".cart-counter");
   cartIconCounter.textContent = cartQuantity;
 }
-
