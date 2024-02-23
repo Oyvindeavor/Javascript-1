6ed827c8c279a8738e22e28660e8b75862e10a30
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-23 23:27:45 +0100

    Update product.mjs

--- product/product.mjs
+++ product/product.mjs
@@ -3,6 +3,8 @@ import { updateCartIcon } from "../scripts/utils/Cart.mjs";
 import { getProducts } from "../scripts/utils/fetchdata.mjs";
 import { createClass,createElement } from "../scripts/utils/domUtils.mjs";
 import { addToCart } from "../scripts/utils/Cart.mjs";
+
+
 // Gets the ID from the url
 export function getIdFromUrl() {
   const filterUrl = new URLSearchParams(window.location.search);
@@ -12,22 +14,26 @@ export function getIdFromUrl() {
 
 // Function to generate product HTML
 function generateProductDisplay(product) {
+  // Creating the main container
   const productContainer = createClass(createElement("div"), "product-page-container");
 
+  // Creating the product Image container
   const productImageContainer = createClass(createElement("div"), "product-page-image");
   productImageContainer.appendChild(createElement("img")).src = product.image;
   productImageContainer.querySelector("img").alt = product.title;
 
+  // Creating the product page details container
   const productDetails = createClass(createElement("div"), "product-page-details");
   productDetails.appendChild(createClass(createElement("h1"), "product-page-title")).textContent = product.title;
   productDetails.appendChild(createClass(createElement("p"), "age-rating")).textContent =  `Age rating: ${product.ageRating}`;
   productDetails.appendChild(createClass(createElement("p"), "genre-text")).textContent = `Genre: ${product.genre}`;
   productDetails.appendChild(createClass(createElement("p"), "released")).textContent = `Released: ${product.released}`;
-
   productDetails.appendChild(createClass(createElement("p"), "product-page-description")).textContent = product.description;
+  // Checks to see if the product is onSale
   if (product.onSale === true){
-    productDetails.appendChild(createClass(createElement("p"),"regular-price-discount-price")).textContent = `${product.discountedPrice}$`
-    productDetails.appendChild(createClass(createElement("p"), "product-discounted-price")).textContent = `${product.price}$`
+    // If its true create and append discounted price and 
+    productDetails.appendChild(createClass(createElement("p"),"regular-price-discount-price")).textContent = `${product.price}$`
+    productDetails.appendChild(createClass(createElement("p"), "product-discounted-price")).textContent = `${product.discountedPrice}$`
   } else {
     productDetails.appendChild(createClass(createElement("p"), "product-page-price")).textContent = `${product.price}$`
   }
@@ -42,17 +48,17 @@ function generateProductDisplay(product) {
   return productContainer;
 }
 
+
 // Function to append product Product HTML elements to the DOM
 function appendProductToDOM(productElement) {
   document.querySelector("main").appendChild(productElement);
 }
 
+
 // Async function to display product
 export async function displayProduct(productId) {
   try {
       const products = await getProducts(); // Fetch all products
-      console.log(getProducts());
-      
       for (const product of products) {
           if (!productId || product.id === productId) {
               const productElement = generateProductDisplay(product);
