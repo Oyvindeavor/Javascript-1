14f9016d863882080c3e06e87086915005e7c851
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-24 12:30:34 +0100

    Update fetchdata.mjs

--- scripts/utils/fetchdata.mjs
+++ scripts/utils/fetchdata.mjs
@@ -1,28 +1,31 @@
-// Import loading spinners 
+// Import loading spinners
+import { displayErrorMessage } from "./errorUserMessage.mjs";
 import { showLoadingSpinner, hideLoadingSpinner } from "./loadingSpinner.mjs";
-
 
 export const api_url = "https://api.noroff.dev/api/v1/gamehub";
 
 // Fetch product data
 export async function getProducts() {
-  // Start the loading spinner first 
-  showLoadingSpinner(); 
+  // Start the loading spinner first
+  showLoadingSpinner();
   try {
     // Fetch, throw an error if the response fail
     const response = await fetch(api_url);
     if (!response.ok) {
+      displayErrorMessage(`Http error: ${response.status}. Try refreshing the page or check your network.`)
       throw new Error(`HTTP error! Status: ${response.status}`);
+      
     }
     // Get back the data in JSON
     const products = await response.json();
-    // Return it so i can reuse it 
-    return products; 
+    // Return it so i can reuse it
+    return products;
   } catch (error) {
-    console.error("Error fetching products:", error.message);
+    displayErrorMessage("We're having trouble fetching the products. Please try again later.")
+    console.error("Error fetching products:", error);
     return []; // return empty to not get undefined error
   } finally {
     // At the end when all is done hide the loading spinner
-    hideLoadingSpinner(); 
+    hideLoadingSpinner();
   }
 }
