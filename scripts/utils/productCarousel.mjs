3faf4977287064f162edcf9d9fba9d8eff10ea34
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-24 20:06:04 +0100

    Update productCarousel.mjs

--- scripts/utils/productCarousel.mjs
+++ scripts/utils/productCarousel.mjs
@@ -1,19 +1,6 @@
 import { createClass, createElement } from "./domUtils.mjs"
+import { getProducts } from "./fetchdata.mjs";
 
-// Create image carousel
-function generateImageCarouselHmtl(){
-    const productCarouselContainer = createClass(createElement("div"), "carousel");
-
-    const carouselItems = createClass(createElement("div"), "carousel-items");
-
-    const carouselItem = createClass(createElement("div"), "carousel-item");
-    const carouselItem2 = createClass(createElement("div"), "carousel-item");
-    const carouselItem3 = createClass(createElement("div"), "carousel-item");
-    const carouselItem4 = createClass(createElement("div"), "carousel-item");
-
-    const previousButton = createClass(createElement("button"), "prev-button");
-    const nextButton = createClass(createElement("button"), "next-button");
-
+async function generateProductCarousel(){
     
-
 }
