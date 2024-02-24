769c8414ebd6b7c3987eac89b5f7db1154dd1ab7
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-24 14:31:44 +0100

    Update CategoryButtons.mjs

--- scripts/utils/CategoryButtons.mjs
+++ scripts/utils/CategoryButtons.mjs
@@ -1,6 +1,8 @@
 import { getProducts } from "../utils/fetchdata.mjs";
 import { createElement, createClass } from "../utils/domUtils.mjs";
 import { displayProducts } from "../../products/products.mjs";
+import { hideLoadingSpinner, showLoadingSpinner } from "./loadingSpinner.mjs";
+import { displayErrorMessage } from "./errorUserMessage.mjs";
 
 // This fetches product genres
 export async function getProductGenres() {
@@ -41,12 +43,14 @@ async function createShowAllCategoryButton(container) {
   }
 }
 
+// Creates category buttons 
 function createCategoryButton(genre, container) {
   const categoryButton = createClass(createElement("button"), "category-button");
   categoryButton.textContent = genre;
   categoryButton.dataset.genre = genre;
   container.appendChild(categoryButton);
   categoryButton.addEventListener("click", async function () {
+    showLoadingSpinner();
     try {
       const products = await getProducts();
       const filteredProducts = filterProductsByGenre(products, genre);
@@ -54,12 +58,20 @@ function createCategoryButton(genre, container) {
       displayProducts(filteredProducts);
     } catch (error) {
       console.error("Could not handle category button click:", error);
+      displayErrorMessage("There was a problem displaying category buttons")
+    } finally{
+        hideLoadingSpinner();
     }
   });
 }
 
+// Filter the products by genre
 export function filterProductsByGenre(products, genre) {
-  return products.filter((product) => product.genre === genre);
+    try{
+        return products.filter((product) => product.genre === genre);
+    } catch(error){
+        console.error("Problem filtering products by genre: ", error)
+    }
 }
 
 export function clearProducts(container) {
