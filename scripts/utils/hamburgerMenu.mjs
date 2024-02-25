f185218bb7d6d31713bdb90b70242ff41bd5d4ca
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-25 21:59:21 +0100

    Update hamburgerMenu.mjs

--- scripts/utils/hamburgerMenu.mjs
+++ scripts/utils/hamburgerMenu.mjs
@@ -1,12 +1,12 @@
 export function setupHamburgerMenu() {
-    document.addEventListener('DOMContentLoaded', function() {
-        const menuBtn = document.getElementById('menuBtn');
-        const navMenu = document.getElementById('navMenu');
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menuBtn");
+    const navMenu = document.getElementById("navMenu");
 
-        if (menuBtn) {
-            menuBtn.addEventListener('click', function() {
-                navMenu.classList.toggle('open');
-            });
-        }
-    });
+    if (menuBtn) {
+      menuBtn.addEventListener("click", function () {
+        navMenu.classList.toggle("open");
+      });
+    }
+  });
 }
