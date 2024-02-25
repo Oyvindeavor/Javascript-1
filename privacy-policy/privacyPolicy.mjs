ecf3047db64b8704a8790ca95c5d42c44595e4f6
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-25 20:12:13 +0100

    Update privacyPolicy.mjs

--- privacy-policy/privacyPolicy.mjs
+++ privacy-policy/privacyPolicy.mjs
@@ -1,9 +1,10 @@
 
 import { updateCartIcon } from "../scripts/utils/Cart.mjs";
-
+import { setupHamburgerMenu } from "../scripts/utils/hamburgerMenu.mjs";
 
 
 async function main() {
+    setupHamburgerMenu();
     await updateCartIcon();
 }
 
