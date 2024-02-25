34340e9416a1ecbce709458327973a4177e9c8d7
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-25 20:12:29 +0100

    Update termsAndConditions.mjs

--- terms-and-conditions/termsAndConditions.mjs
+++ terms-and-conditions/termsAndConditions.mjs
@@ -1,9 +1,11 @@
 
 import { updateCartIcon } from "../scripts/utils/Cart.mjs";
+import { setupHamburgerMenu } from "../scripts/utils/hamburgerMenu.mjs";
 
 
 
 async function main() {
+    setupHamburgerMenu();
     await updateCartIcon();
 }
 
