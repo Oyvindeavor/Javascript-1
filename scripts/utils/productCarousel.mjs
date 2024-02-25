fe98aaab38fb65519ae2f1b8ab9acdc714b681c5
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-25 21:59:26 +0100

    Delete productCarousel.mjs

--- scripts/utils/productCarousel.mjs
+++
@@ -1,9 +0,0 @@
-import { createClass, createElement } from "./domUtils.mjs"
-import { getProducts } from "./fetchdata.mjs";
-
-export async function generateProductCarousel(){
-    const mainElement = document.querySelector("main");
-    const itemsContainer = createClass(createElement("div"), ".items-container");
-
-    
-}
