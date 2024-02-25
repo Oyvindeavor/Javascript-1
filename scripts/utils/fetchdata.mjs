fa23b9f267ecc658732538528a29fdc0ce6596e9
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-25 21:59:18 +0100

    Update fetchdata.mjs

--- scripts/utils/fetchdata.mjs
+++ scripts/utils/fetchdata.mjs
@@ -10,15 +10,15 @@ export async function getProducts() {
   try {
     const response = await fetch(api_url);
     if (!response.ok) {
-      displayErrorMessage(`Http error: ${response.status}. Try refreshing the page or check your network.`)
+      displayErrorMessage(`Http error: ${response.status}. Try refreshing the page or check your network.`);
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const products = await response.json();
     return products;
   } catch (error) {
-    displayErrorMessage("We're having trouble fetching the products. Please try again later.")
+    displayErrorMessage("We're having trouble fetching the products. Please try again later.");
     console.error("Error fetching products:", error);
-    return []; 
+    return [];
   } finally {
     hideLoadingSpinner();
   }
