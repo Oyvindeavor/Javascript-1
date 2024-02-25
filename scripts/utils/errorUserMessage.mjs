53928aa4fde03d87a023701285adf14f41caae57
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-25 21:59:16 +0100

    Update errorUserMessage.mjs

--- scripts/utils/errorUserMessage.mjs
+++ scripts/utils/errorUserMessage.mjs
@@ -1,13 +1,11 @@
-
 // Shows error message in the middle of the screen with a timeout.
 export function displayErrorMessage(message) {
-    const errorElement = document.createElement('div');
-    errorElement.textContent = message;
-    errorElement.className = 'error-message'; 
-    const notificationArea = document.querySelector('body') 
-    notificationArea.appendChild(errorElement);
-    setTimeout(() => {
-      errorElement.remove();
-    }, 5000); 
-  }
-  
+  const errorElement = document.createElement("div");
+  errorElement.textContent = message;
+  errorElement.className = "error-message";
+  const notificationArea = document.querySelector("body");
+  notificationArea.appendChild(errorElement);
+  setTimeout(() => {
+    errorElement.remove();
+  }, 5000);
+}
