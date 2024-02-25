7d7d4f6e284323d41f651037de0d8f8c3da7bba6
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-02-25 21:59:23 +0100

    Update loadingSpinner.mjs

--- scripts/utils/loadingSpinner.mjs
+++ scripts/utils/loadingSpinner.mjs
@@ -1,14 +1,7 @@
-/**
- * Shows a loading spinner by modifying the main cursor to "wait" by using body.style.
- */
-
 export function showLoadingSpinner() {
   document.body.style.cursor = "wait";
 }
 
-/**
- * Hides the loading spinner by setting the cursor body.style to "auto".
- */
 export function hideLoadingSpinner() {
   document.body.style.cursor = "auto";
 }
